3fef50f707e7ae6ca013c42f46c656b790ab9fcf
anhhaidayx <80026737+anhhaidayx@users.noreply.github.com>
2021-07-21 15:14:05 +0700

    Add files via upload

---
+++ file_python.py
@@ -0,0 +1,13 @@
+#xu li File 
+file_obj = open('filepython.txt')
+
+data= file_obj.read(2)
+print(data)
+
+
+itera1 = {1,5,4,6}
+print(itera1)
+print(sum(itera1))
+
+#Nhap trong python
+input('hao') 
